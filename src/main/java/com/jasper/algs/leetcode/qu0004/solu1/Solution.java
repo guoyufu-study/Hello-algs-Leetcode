846ef62f2a872a84612150a02a0698a02e7c4c2c
jasper <guoyufu_study@163.com>
2020-02-28 16:38:28 +0800

    寻找两个有序数组的中位数

--- src/main/java/com/jasper/algs/leetcode/qu0004/solu1/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/qu0004/solu1/Solution.java
@@ -1,9 +1,46 @@
 package com.jasper.algs.leetcode.qu0004.solu1;
 
+/**
+ * 二分查找+中位数计算
+ */
 public class Solution {
 
 	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		int m = nums1.length;
+		int n = nums2.length;
 		
-		return 0;
+		if(m>n) {//确保 m<=n
+			int[] tmp = nums1; nums1 = nums2; nums2 = tmp;
+			m = nums1.length;
+			n = nums2.length;
+		}
+		
+		if(n==0) // 两数组都无值
+			throw new IllegalArgumentException("两个数组全部无值...");
+		
+		// 确定 i 和 j
+		int imin=0, imax=m, halfLen=(m+n+1)/2;
+		while(imin<=imax) {//二分查找=>确定i
+			int i=(imin+imax)/2;
+			int j = halfLen - i;
+			if(i<imax && nums2[j-1]>nums1[i]) imin=i+1;//小了，右移
+			else if(i>imin && nums1[i-1]>nums2[j]) imax=i-1;//大了，左移
+			else break;
+		}
+		int i=(imin+imax)/2;
+		int j = halfLen-i;//确定j
+		
+		
+		//计算中位数，并返回
+		int L= i==0 ? nums2[j-1]:
+							j==0? nums1[m-1] : //i==m，且m=n时，可能导致j=0
+								Math.max(nums1[i-1], nums2[j-1]);
+							
+		if((m+n)%2==1) return L;
+		
+		int R= i==m ? nums2[j] :
+							j==n ? nums1[0] : //i==0，且m=n时，会导致j=n
+								Math.min(nums1[i], nums2[j]);
+		return (L+R)/2.0;
 	}
 }
---
+++ src/test/java/com/jasper/algs/leetcode/qu0004/SolutionTest.java
@@ -0,0 +1,42 @@
+package com.jasper.algs.leetcode.qu0004;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.jasper.algs.leetcode.qu0004.solu1.Solution;
+
+class SolutionTest {
+
+	@ParameterizedTest
+	@MethodSource("data")
+	void testFindMedianSortedArrays(int[] nums1, int[] nums2, double expected) {
+		double median = new Solution()
+			.findMedianSortedArrays(nums1, nums2);
+		assertThat(median).isEqualTo(expected);
+	}
+	
+	static Stream<Arguments> data() {
+		return Stream.of(
+				arguments(new int[] {}, new int[] {2,3}, 2.5),
+				arguments(new int[] {2,3}, new int[] {}, 2.5),
+				
+				arguments(new int[] {2,3}, new int[] {1}, 2),
+				arguments(new int[] {1,3}, new int[] {2}, 2),
+				arguments(new int[] {1,2}, new int[] {3}, 2),
+				
+				arguments(new int[] {1,2}, new int[] {3,4}, 2.5),
+				arguments(new int[] {3,4}, new int[] {1,2}, 2.5),
+				
+				arguments(new int[] {1,2,3}, new int[] {4,5,6}, 3.5),
+				arguments(new int[] {5,6,7,8}, new int[] {1,2,3,4}, 4.5)
+				
+				);
+	}
+
+}
