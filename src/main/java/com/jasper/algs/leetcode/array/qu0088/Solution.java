b39e25f0ce1789d6f5e174fe2fd94f34f6de3275
guoyufu-study <guoyufu_study@163.com>
2020-04-08 02:41:44 +0800

    0088.合并两个有序数组 => 数组、双指针

---
+++ src/main/java/com/jasper/algs/leetcode/array/qu0088/Solution.java
@@ -0,0 +1,17 @@
+package com.jasper.algs.leetcode.array.qu0088;
+
+/**
+ * 双指针：从后往前
+ */
+class Solution {
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		while(m!=0 && n !=0) {
+			nums1[m+n-1] = nums1[m-1]>nums2[n-1] ? nums1[--m] : nums2[--n];
+		}
+		
+		if(m==0) 
+			while(n!=0) nums1[n-1] = nums2[--n];
+		else if(n==0)
+			while(m!=0) nums1[m-1] = nums1[--m];
+    }
+}
