6c694ea69ab8dceb969f3a521433644ace70829e
guoyufu-study <guoyufu_study@163.com>
2020-04-07 13:15:10 +0800

    0232 用栈实现队列 总结

--- src/main/java/com/jasper/algs/leetcode/stack/qu0232/solu2/MyQueue.java
+++ src/main/java/com/jasper/algs/leetcode/stack/qu0232/solu2/MyQueue.java
@@ -1,7 +1,10 @@
-package com.jasper.algs.leetcode.stack.qu0232;
+package com.jasper.algs.leetcode.stack.qu0232.solu2;
 
 import java.util.Stack;
 
+/**
+ * 两个栈 入队 - O(1)，出队 - 摊还复杂度 O(1)
+ */
 class MyQueue {
 	
 	private Stack<Integer> outStack;
