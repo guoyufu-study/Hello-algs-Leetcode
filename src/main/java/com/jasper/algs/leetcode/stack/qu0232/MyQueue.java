7a74835947d6ac287df004dc17c01939cd393bf0
guoyufu-study <guoyufu_study@163.com>
2020-04-07 12:21:35 +0800

    0232 用栈实现队列

---
+++ src/main/java/com/jasper/algs/leetcode/stack/qu0232/MyQueue.java
@@ -0,0 +1,49 @@
+package com.jasper.algs.leetcode.stack.qu0232;
+
+import java.util.Stack;
+
+class MyQueue {
+	
+	private Stack<Integer> outStack;
+	private Stack<Integer> inStack;
+	
+    /** 初始化数据结构 */
+    public MyQueue() {
+    	inStack = new Stack<Integer>();
+    	outStack = new Stack<Integer>();
+    }
+    
+    /** 入队 */
+    public void push(int x) {
+    	inStack.push(x);
+    }
+    
+    /** 出队 */
+    public int pop() {
+    	if(outStack.isEmpty())
+    		while(!inStack.isEmpty())
+    			outStack.push(inStack.pop());
+    	return outStack.pop();
+    }
+    
+    /** 队首元素  */
+    public int peek() {
+    	if(outStack.isEmpty())
+    		while(!inStack.isEmpty())
+    			outStack.push(inStack.pop());
+    	return outStack.peek();
+    }
+    
+    /** 判空   */
+    public boolean empty() {
+    	return outStack.isEmpty() && inStack.isEmpty();
+    }
+}
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
