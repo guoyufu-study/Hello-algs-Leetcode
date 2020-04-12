86091959ae6ad69f12ce035a87e21fffb8412104
guoyufu-study <guoyufu_study@163.com>
2020-04-12 20:18:28 +0800

    0013. 罗马数字转整数

---
+++ src/main/java/com/jasper/algs/leetcode/maths/qu0013/solu1/Solution.java
@@ -0,0 +1,43 @@
+package com.jasper.algs.leetcode.maths.qu0013.solu1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 使用HashMap
+ */
+class Solution {
+	
+	private static final Map<Character, Integer> map;
+	
+	static {
+		map = new HashMap<Character, Integer>();
+		map.put('I', 1);
+		map.put('V', 5);
+		map.put('X', 10);
+		map.put('L', 50);
+		map.put('C', 100);
+		map.put('D', 500);
+		map.put('M', 1000);
+	}
+	
+    public int romanToInt(String s) {
+        int ans = 0;
+        int prev = map.get(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+			int num = map.get(s.charAt(i));
+			ans = prev>=num?ans+prev:ans-prev;
+			prev = num;
+        	
+		}
+        ans += prev;
+        return ans;
+    }
+
+ 
+ 
+    public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/main/java/com/jasper/algs/leetcode/maths/qu0013/solu2/Solution.java
@@ -0,0 +1,32 @@
+package com.jasper.algs.leetcode.maths.qu0013.solu2;
+
+/**
+ * 替代HashMap
+ *
+ */
+class Solution {
+    public int romanToInt(String s) {
+        int ans = 0;
+        int prev = getValue(s.charAt(0));
+        for(int i = 1;i < s.length(); i ++) {
+            int num = getValue(s.charAt(i));
+            ans = prev<num?ans-prev:ans+prev;
+            prev = num;
+        }
+        ans += prev;
+        return ans;
+    }
+    
+    private int getValue(char ch) {
+        switch(ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+}
