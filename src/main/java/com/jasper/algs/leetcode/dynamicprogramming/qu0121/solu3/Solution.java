b968b33adcfb50959c34e4aeab418d071df68b44
guoyufu-study <guoyufu_study@163.com>
2020-04-13 15:12:08 +0800

    121. 买卖股票的最佳时机 => 动态规划

---
+++ src/main/java/com/jasper/algs/leetcode/dynamicprogramming/qu0121/solu3/Solution.java
@@ -0,0 +1,28 @@
+package com.jasper.algs.leetcode.dynamicprogramming.qu0121.solu3;
+
+/**
+ * 一次遍历
+ */
+class Solution {
+    public int maxProfit(int[] prices) {
+    	
+    	// 边界值
+    	if(prices==null || prices.length==0) return 0;
+    	
+    	int ans = 0; // 不买卖时，收益
+    	
+    	int min = prices[0];
+    	for (int i = 1; i < prices.length; i++) {
+			if(prices[i]<min)
+				min = prices[i];//更新最小值
+			else {
+				int profit = prices[i]-min;
+				if(profit>ans) //更新最大收益
+					ans = profit;
+			}
+		}
+    	
+    	return ans;
+    	
+    }
+}
