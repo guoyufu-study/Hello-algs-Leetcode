3e363e64014272e830bde2c76173412f43a6c0a8
guoyufu-study <guoyufu_study@163.com>
2020-04-15 15:31:26 +0800

    17.19. 消失的两个数字 => 位运算

---
+++ src/main/java/com/jasper/algs/interview/q1719/solu1/Solution.java
@@ -0,0 +1,26 @@
+package com.jasper.algs.interview.q1719.solu1;
+
+/**
+ * 暴力求解：逐个比较
+ */
+class Solution {
+    public int[] missingTwo(int[] nums) {
+    	int[] ans = new int[2];
+    	
+    	int count = 0;
+    	boolean missing = true;
+		for (int i = 1; i <= ans.length + 2; i++) {
+			for (int j = 0; j < ans.length; j++) { // 逐个比较
+				if(i==nums[j]) {
+					missing = false;//存在，不缺失
+					break;
+				}
+			}
+			if(missing) ans[count++] = i; // 记录缺失的数字
+			if(count==2) break; // 全部找到
+			missing = true;
+		}
+    	
+    	return ans;
+    }
+}
---
+++ src/main/java/com/jasper/algs/interview/q1719/solu4/Solution.java
@@ -0,0 +1,42 @@
+package com.jasper.algs.interview.q1719.solu4;
+
+/**
+ * 4. 位运算
+ * 
+ * <p>关键在于，如何排除其中一个缺失数字，将问题转变成求消失的一个数问题
+ */
+class Solution {
+    public int[] missingTwo(int[] nums) {
+    	
+    	// 求x^y
+    	int xor = 0;
+    	for (int i = 0; i < nums.length; i++) {
+    		xor ^= nums[i];
+		}
+    	int N = nums.length+2;
+		for (int i = 1; i <= N; i++) {
+			xor ^= i;
+		}
+		
+		//求xy不同:最右侧的1
+    	int diff = xor & -xor; 
+    	
+		// 排除y，求x
+    	int x = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if((diff & nums[i]) == 0)
+				x ^= nums[i];
+		}
+		for (int i = 1; i <= N; i++) {
+			if((diff & i) == 0) x ^= i;
+		}
+		
+    	
+    	return new int[] {x, xor^x};
+    }
+    
+    
+    public static void main(String[] args) {
+    	
+	}
+}
