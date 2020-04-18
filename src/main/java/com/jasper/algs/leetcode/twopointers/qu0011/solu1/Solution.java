ad3b2566354a2dfa13a31213ae05b50fee6e6943
guoyufu-study <guoyufu_study@163.com>
2020-04-18 19:02:57 +0800

    0011.盛水最多的容器

--- src/main/java/com/jasper/algs/leetcode/twopointers/qu0011/solu1/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/twopointers/qu0011/solu1/Solution.java
@@ -1,14 +1,14 @@
 package com.jasper.algs.leetcode.twopointers.qu0011.solu1;
 
 /**
- * 暴力求解
+ * 暴力求解：尝试所有可能组合
  */
 class Solution {
 
 	public int maxArea(int[] height) {
 		int ans = 0;
 		for (int i = 0; i < height.length; i++) {
-			for (int j = i+1; j < height.length; j++) {
+			for (int j = i+1; j < height.length; j++) {// 尝试所有可能组合
 				ans = Math.max(ans, (j-i)*Math.min(height[i], height[j]));
 			}
 		}
