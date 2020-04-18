2a2585cbce5d86b28f845561a62f1227bb30f456
guoyufu-study <guoyufu_study@163.com>
2020-04-18 19:15:20 +0800

    0167.两数之和 II - 输入有序数组

--- src/main/java/com/jasper/algs/leetcode/twopointers/qu0167/solu2/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/twopointers/qu0167/solu2/Solution.java
@@ -5,15 +5,33 @@ package com.jasper.algs.leetcode.twopointers.qu0167.solu2;
  */
 public class Solution {
 	
-	public int[] twoSum(int[] nums, int target) {
+	public int[] twoSum(int[] numbers, int target) {
+		// 数组长度不符合要求
+		int n = numbers.length;
+		if(n<2) 
+			throw new IllegalArgumentException("No two sum solution");
+
+		// 最小值仍然大了
+		int minSum = numbers[0]+numbers[1];
+		if(minSum == target) return new int[] {1, 2};
+		else if(minSum>target) throw new IllegalArgumentException("No two sum solution");
+		
+		// 最大值仍然小了
+		int maxSum = numbers[n-1]+numbers[n-2];
+		if(maxSum == target) return new int[] {n-1, n};
+		else if(maxSum<target) throw new IllegalArgumentException("No two sum solution");
+		
+		
 		// 每个输入只对应唯一的答案 => 不会有重复元素 => 不用考虑去重问题
-		int L=0, R=nums.length-1;
+		int L=0, R=n-1;
 		while(L<R) {
-			int sum = nums[L]+nums[R];//求和，不考虑溢出问题
+			int sum = numbers[L]+numbers[R];//求和，不考虑溢出问题
 			if(sum==target) return new int[] {L+1,R+1};
 			else if(sum<target) L++;//小了
 			else R--;//大了
 		}
+		
+		// 没有结果
 		throw new IllegalArgumentException("No two sum solution");
 	}
 }
