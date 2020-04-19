97885083ddcf607fe78009a31410006acb11bd2d
guoyufu-study <guoyufu_study@163.com>
2020-04-19 16:02:07 +0800

    0016.最接近的三数之和

--- src/main/java/com/jasper/algs/leetcode/twopointers/qu0016/solu1/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/twopointers/qu0016/solu1/Solution.java
@@ -12,7 +12,7 @@ public class Solution {
 		int ans=nums[0]+nums[1]+nums[2];
 		for (int i = 0; i < nums.length; i++) {//三层循环
 			for (int j = i+1; j < nums.length; j++) {
-				for (int k = j+1; k < nums.length; k++) {
+				for (int k = j+1; k < nums.length; k++) {// 尝试所有可能的组合
 					int sum = nums[i]+nums[j]+nums[k];//求和
 					if(sum==target) return target; //最接近值 target
 					if(Math.abs(target-sum)<Math.abs(target-ans)) //更新答案
