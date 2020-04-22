111badc42e54892c7e482636a2773f1913f46a38
guoyufu-study <guoyufu_study@163.com>
2020-04-22 14:44:53 +0800

    0070.爬楼梯 => 动态规划、斐波那契数

---
+++ src/main/java/com/jasper/algs/leetcode/dynamicprogramming/qu0070/solu1/Solution.java
@@ -0,0 +1,34 @@
+package com.jasper.algs.leetcode.dynamicprogramming.qu0070.solu1;
+
+/**
+ * 暴力求解：
+ * 
+ * <p> 尝试所有可能的组合，成功+1
+ */
+class Solution {
+    public int climbStairs(int n) {
+    	if(n<=1) 
+    		return n;
+    	return climbStairs(0, n);
+    }
+    
+    /**
+     * @param i 当前位于第i层台阶
+     * @param n 目标总共有n层台阶
+     * @return 从第i层台阶开始爬楼，总共有几种方法
+     */
+    private int climbStairs(int i, int n) {
+    	if(i > n)
+    		return 0;
+    	if(i == n)
+    		return 1;
+    	
+    	return climbStairs(i+1, n) + climbStairs(i+2, n);
+    }
+    
+    
+    public static void main(String[] args) {
+		int n = 4;
+		System.out.println(new Solution().climbStairs(n ));
+	}
+}
---
+++ src/main/java/com/jasper/algs/leetcode/dynamicprogramming/qu0070/solu2/Solution.java
@@ -0,0 +1,30 @@
+package com.jasper.algs.leetcode.dynamicprogramming.qu0070.solu2;
+
+/**
+ * 动态规划
+ * 
+ * <p>
+ * 第 i 阶可以由两种方法得到：
+ * <ol>
+ * <li>在第 (i-1) 阶后向上爬1阶。
+ * <li>在第 (i-2) 阶后向上爬 2阶。
+ * </ol>
+ * <p>所以，F(i) = F(i-1) + F(i-2);
+ */
+class Solution {
+    public int climbStairs(int n) {
+    	if(n<=1) 
+    		return n;
+    	
+    	int[] dp = new int[n+1];
+    	dp[1] = 1;
+    	dp[2] = 2;
+    	
+    	for (int i = 3; i <= n; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+    	
+    	return dp[n];
+    }
+    
+}
