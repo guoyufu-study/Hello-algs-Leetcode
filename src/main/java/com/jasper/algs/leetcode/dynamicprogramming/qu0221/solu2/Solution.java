0594f8c299fc8cfe6b6a00d7a280f9238c76f4c7
guoyufu-study <guoyufu_study@163.com>
2020-04-23 00:31:24 +0800

    0221.最大正方形 => 动态规划

---
+++ src/main/java/com/jasper/algs/leetcode/dynamicprogramming/qu0221/solu2/Solution.java
@@ -0,0 +1,32 @@
+package com.jasper.algs.leetcode.dynamicprogramming.qu0221.solu2;
+
+/**
+ * 动态规划（优化）
+ */
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+    	if(matrix.length==0) return 0; // 边界值
+    	
+        int rows = matrix.length, 
+        	cols = matrix[0].length; //行列
+       
+        // 辅助数组
+        int[] dp = new int[cols + 1];
+        int maxsqlen = 0, prev = 0;
+        
+        // 动态规划
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                int temp = dp[j];
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[j] = Math.min(Math.min(dp[j - 1], prev), dp[j]) + 1;//更新dp[j]，也就是下次计算用到的dp[j-1]
+                    maxsqlen = Math.max(maxsqlen, dp[j]);//更新最大边长
+                } else {
+                    dp[j] = 0;
+                }
+                prev = temp;//更新下次要用的prev，也就是这次用的到dp[j]
+            }
+        }
+        return maxsqlen * maxsqlen;
+    }
+}
