eaea2df65134762740ab2d006b94982dd51c28dd
guoyufu-study <guoyufu_study@163.com>
2020-05-04 19:13:14 +0800

    0495. 提莫攻击

---
+++ src/main/java/com/jasper/algs/leetcode/interval/qu0495/solu1/Solution.java
@@ -0,0 +1,26 @@
+package com.jasper.algs.leetcode.interval.qu0495.solu1;
+
+/**
+ * 0495. 提莫攻击
+ *
+ */
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+    	
+    	// 边界
+    	if(timeSeries==null || timeSeries.length==0)
+    		return 0;
+    	if(duration==0) return 0;
+    	
+    	int N = timeSeries.length;
+    	
+    	// 累计时间
+    	int ans = 0;
+    	for (int i = 1; i < N; i++) {
+			ans += Math.min(duration, timeSeries[i]- timeSeries[i-1]);
+		}
+    	
+    	// 注意最后一击
+    	return ans+duration;
+    }
+}
