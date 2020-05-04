2c5dc785920aff2e8a9b32760c759ea5a3e661e7
guoyufu-study <guoyufu_study@163.com>
2020-05-04 12:52:24 +0800

    删除重复元素问题

--- src/main/java/com/jasper/algs/leetcode/twopointers/qu0026/solu1/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/twopointers/qu0026/solu1/Solution.java
@@ -1,25 +1,32 @@
 package com.jasper.algs.leetcode.twopointers.qu0026.solu1;
 
 /**
- * 拷贝覆盖
+ * 0026.删除有序数组中的重复项
+ * 
+ * <p> 双指针：拷贝覆盖
  */
 public class Solution {
 
 	public int removeDuplicates(int[] nums) {
 
-		if(nums.length==1) return 1;
+		// 边界
+		int k = 1;
+		if(nums==null) return 0;
+		if(nums.length<=k) return nums.length;
 		
-		int i = 1;
-		for (; i < nums.length; i++) {
-			if(nums[i]==nums[i-1]) break;
+		int N = nums.length;
+		
+		// 无需修改部分：跳过
+		int i = k;
+		for (; i < N; i++) {
+			if(nums[i]==nums[i-k]) break;
 		}
 		
+		// 需要修改部分：拷贝覆盖
 		int ans = i;
-		for (; i < nums.length; i++) {
-			if(nums[i]!=nums[i-1]) {
+		for (; i < N; i++)
+			if(nums[i]!=nums[i-1])
 				nums[ans++]=nums[i];
-			}
-		}
 		
 		return ans;
     }
--- src/main/java/com/jasper/algs/leetcode/twopointers/qu0080/solu1/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/twopointers/qu0080/solu1/Solution.java
@@ -1,26 +1,31 @@
 package com.jasper.algs.leetcode.twopointers.qu0080.solu1;
 
 /**
- * 拷贝覆盖
+ * 0080.删除有序数组中的重复项II
+ * 
+ * <p>
+ * 同向指针：拷贝覆盖
  */
 public class Solution {
 
 	public int removeDuplicates(int[] nums) {
-
-		if(nums==null) return 0;
-		else if(nums.length<=2) return nums.length;
 		
-		//双指针
+		int k = 2;
+		
+		// 边界
+		if(nums==null) return 0;
+		if(nums.length<=k) return nums.length;
+		
+		// 跳到第一个要移除的元素的位置
 		int i=2;
-		for (; i < nums.length; i++) // 跳到第一个要移除的元素的位置
-			if(nums[i]==nums[i-2]) break;
+		for (; i < nums.length; i++)
+			if(nums[i]==nums[i-k]) break;
 			
+		// 同向指针：拷贝覆盖
 		int ans=i;
-		for (; i < nums.length; i++) {
-			if(nums[i]!=nums[ans-2]) {
+		for (; i < nums.length; i++)
+			if(nums[i]!=nums[ans-2])
 				nums[ans++] = nums[i];
-			}
-		}
 		
 		return ans;
     }
