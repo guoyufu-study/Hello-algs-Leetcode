37a22ba39affbedb70c3bc3da3a572e1f8aeba63
guoyufu-study <guoyufu_study@163.com>
2020-05-13 17:35:27 +0800

    只出现过一次的数字 => 位运算

--- src/main/java/com/jasper/algs/leetcode/bitmanipulation/qu0137/solu3/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/bitmanipulation/qu0137/solu3/Solution.java
@@ -1,5 +1,10 @@
 package com.jasper.algs.leetcode.bitmanipulation.qu0137.solu3;
 
+/**
+ * 0137. 只出现过一次的数字2
+ * 
+ * <p>位掩码
+ */
 public class Solution {
 
 	public int singleNumber(int[] nums) {
@@ -8,9 +13,15 @@ public class Solution {
 			// 第一次出现，将num添加到seenOnce，seenTwice不变
 			// 第二次出现，将num移除出seenOnce，将num添加到seenTwice；
 			// 第三次出现，seenOnce不变，将num移除出seenTwice；
-			seenOnce = ~seenTwice & (seenOnce ^ num);
+			seenOnce = (seenOnce ^ num) & ~seenTwice;
 			seenTwice = ~seenOnce & (seenTwice ^ num);
+			
 		}
 		return seenOnce;
     }
+	
+	public static void main(String[] args) {
+		int[] nums = new int[] {2,2,3,2};
+		System.out.println(new Solution().singleNumber(nums));
+	}
 }
