3f7d47f499b8161adbed8b6e4b75e2632b3be387
guoyufu-study <guoyufu_study@163.com>
2020-05-13 19:54:18 +0800

    0191.位1的个数 => 布赖恩·克尼根算法

---
+++ src/main/java/com/jasper/algs/leetcode/bitmanipulation/qu0191/solu1/Solution.java
@@ -0,0 +1,21 @@
+package com.jasper.algs.leetcode.bitmanipulation.qu0191.solu1;
+
+/**
+ * 0191.位1的个数
+ * 
+ * <p>32次位掩码
+ */
+public class Solution {
+    // you need to treat n as an unsigned value
+	public int hammingWeight(int n) {
+	    int bits = 0;
+	    int mask = 1;
+	    for (int i = 0; i < 32; i++) {
+	        if ((n & mask) != 0) {
+	            bits++;
+	        }
+	        mask <<= 1;
+	    }
+	    return bits;
+	}
+}
---
+++ src/main/java/com/jasper/algs/leetcode/bitmanipulation/qu0191/solu2/Solution.java
@@ -0,0 +1,18 @@
+package com.jasper.algs.leetcode.bitmanipulation.qu0191.solu2;
+
+/**
+ * 0191.位1的个数
+ * 
+ * <p>布赖恩·克尼根算法
+ */
+public class Solution {
+	// you need to treat n as an unsigned value
+	public int hammingWeight(int n) {
+		int bits = 0;
+		while(n!=0) {
+			bits++;
+			n &= (n-1);
+		}
+		return bits;
+	}
+}
