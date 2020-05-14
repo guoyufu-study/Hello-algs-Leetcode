6266a0d7fdf2bae4c0d009c24b31c30dd3f19199
guoyufu-study <guoyufu_study@163.com>
2020-05-14 15:18:38 +0800

    0053. 最大子序和 =>动态规划、贪心算法、线段树

---
+++ src/main/java/com/jasper/algs/leetcode/dynamicprogramming/qu0053/solu2/Solution.java
@@ -0,0 +1,22 @@
+package com.jasper.algs.leetcode.dynamicprogramming.qu0053.solu2;
+
+/**
+ * 0053. 最大子序和
+ * 
+ * <p> 1. 动态规划
+ */
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums==null || nums.length==0)
+        	return 0;
+        
+    	int ans = nums[0];
+    	int pre = 0;
+        for (int num : nums) {
+			pre = Math.max(pre+num, num);
+			ans = Math.max(ans, pre);
+		}
+        
+        return ans;
+    }
+}
