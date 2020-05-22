06993f565b52a76eec6cea47cfd69c91f855de9e
guoyufu-study <guoyufu_study@163.com>
2020-05-23 00:02:25 +0800

    0232. 用栈实现队列

---
+++ src/main/java/com/jasper/algs/leetcode/stack/qu0232/solu/MyQueue.java
@@ -0,0 +1,46 @@
+package com.jasper.algs.leetcode.stack.qu0232.solu;
+
+/**
+ * 用栈实现队列
+ * 
+ * <p>三刷
+ */
+class MyQueue {
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+    	
+    	return 0;
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+
+    	return 0;
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+
+    	return false;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
--- src/main/java/com/jasper/algs/leetcode/stack/qu0232/solu2/MyQueue.java
+++ src/main/java/com/jasper/algs/leetcode/stack/qu0232/solu2/MyQueue.java
@@ -3,7 +3,9 @@ package com.jasper.algs.leetcode.stack.qu0232.solu2;
 import java.util.Stack;
 
 /**
- * 两个栈 入队 - O(1)，出队 - 摊还复杂度 O(1)
+ * 用栈实现队列
+ * 
+ * <p>两个栈 入队 - O(1)，出队 - 摊还复杂度 O(1)
  */
 class MyQueue {
 	
