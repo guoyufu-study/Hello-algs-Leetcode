2cdcbbedbbb3d05858ac1e3dc7a1c43e42033bfe
guoyufu-study <guoyufu_study@163.com>
2020-05-25 00:34:07 +0800

    0004.寻找两个有序数组的中位数 4刷

--- src/main/java/com/jasper/algs/leetcode/array/qu0004/solu/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/array/qu0004/solu/Solution.java
@@ -3,12 +3,24 @@ package com.jasper.algs.leetcode.array.qu0004.solu;
 /**
  * 0004.寻找两个有序数组的中位数
  * 
- * <p>3刷
+ * <p>4刷
  */
 public class Solution {
 
 	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 		
 		return 0;
+	}
+	
+	public static void main(String[] args) {
+		// 2.0
+		int[] nums1 = new int[] {1,3};
+		int[] nums2 = new int[] {2};
+		
+		// 2.5
+//		int[] nums1 = new int[] {1,2};
+//		int[] nums2 = new int[] {3,4};
+		
+		System.out.println(new Solution().findMedianSortedArrays(nums1, nums2));
 	}
 }
--- src/main/java/com/jasper/algs/leetcode/array/qu0004/solu1/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/array/qu0004/solu1/Solution.java
@@ -8,30 +8,25 @@ package com.jasper.algs.leetcode.array.qu0004.solu1;
 public class Solution {
 
 	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		// 保证 num1.length <= nums2.length
+		if(nums1.length>nums2.length) {
+			int[] tmp = nums1;
+			nums1 = nums2;
+			nums2 = tmp;
+		}
 		int m = nums1.length;
 		int n = nums2.length;
 		
-		if(m>n) {//确保 m<=n
-			int[] tmp = nums1; nums1 = nums2; nums2 = tmp;
-			m = nums1.length;
-			n = nums2.length;
-		}
-		
-		if(n==0) // 两数组都无值
-			throw new IllegalArgumentException("两个数组全部无值...");
-		
 		// 确定 i 和 j
-		int imin=0, imax=m, halfLen=(m+n+1)/2;
-		while(imin<=imax) {//二分查找=>确定i
-			int i=(imax-imin)/2+imin;
-			int j = halfLen - i;
-			if(i<imax && nums2[j-1]>nums1[i]) imin=i+1;//小了，右移
-			else if(i>imin && nums1[i-1]>nums2[j]) imax=i-1;//大了，左移
-			else break;
+		int left=0, right=m, half=(m+n+1)/2;
+		int i=0, j=0;
+		while(left<=right) {//二分查找=>确定i
+			i=(right-left)/2+left;
+			j = half - i;
+			if(i>left && nums1[i-1]>nums2[j]) right=i-1;//大了，左移
+			else if(i<right && nums2[j-1]>nums1[i]) left=i+1;//小了，右移
+			else break; // 找到
 		}
-		int i=(imax-imin)/2+imin;
-		int j = halfLen - i;//确定j
-		
 		
 		//计算中位数，注意边界处理
 		int maxLeft = i==0 ? nums2[j-1]:
@@ -45,5 +40,17 @@ public class Solution {
 								Math.min(nums1[i], nums2[j]);
 		
 		return (maxLeft+minRight)/2.0; // 偶数
+	}
+	
+	public static void main(String[] args) {
+		// 2.0
+//		int[] nums1 = new int[] {1,3};
+//		int[] nums2 = new int[] {2};
+		
+		// 2.5
+		int[] nums1 = new int[] {1,2};
+		int[] nums2 = new int[] {3,4};
+		
+		System.out.println(new Solution().findMedianSortedArrays(nums1, nums2));
 	}
 }
