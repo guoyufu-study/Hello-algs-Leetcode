07da3c46e986d51e542ec27713b972222fb74f1f
guoyufu-study <guoyufu_study@163.com>
2020-05-24 17:46:28 +0800

    跳跃问题

---
+++ src/main/java/com/jasper/algs/leetcode/greedy/qu0045/solu/Solution.java
@@ -0,0 +1,14 @@
+package com.jasper.algs.leetcode.greedy.qu0045.solu;
+
+/**
+ * 0045.跳跃游戏 II
+ *
+ * <p>二刷
+ */
+class Solution {
+    public int jump(int[] nums) {
+    	
+    	
+    	return 0;
+    }
+}
---
+++ src/main/java/com/jasper/algs/leetcode/greedy/qu0055/solu/Solution.java
@@ -0,0 +1,14 @@
+package com.jasper.algs.leetcode.greedy.qu0055.solu;
+
+/**
+ * 0055. 跳跃游戏
+ *
+ * <p>二刷
+ */
+class Solution {
+    public boolean canJump(int[] nums) {
+    	
+    	
+    	return false;
+    }
+}
