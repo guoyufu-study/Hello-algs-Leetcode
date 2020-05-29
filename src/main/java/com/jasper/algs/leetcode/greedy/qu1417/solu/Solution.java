9f8e1cf26214a786a0b94a884bb3635a2e430d34
guoyufu-study <guoyufu_study@163.com>
2020-05-29 12:58:17 +0800

    1417.重新格式化字符串 => 贪心算法

---
+++ src/main/java/com/jasper/algs/leetcode/greedy/qu1417/solu/Solution.java
@@ -0,0 +1,33 @@
+package com.jasper.algs.leetcode.greedy.qu1417.solu;
+
+/**
+ * 1417. 重新格式化字符串
+ * 
+ * <p> 3刷
+ */
+class Solution {
+    public String reformat(String s) {
+    	
+    	return "";
+    }
+    
+    public static void main(String[] args) {
+		
+    	// 1
+    	String s = "a0b1c2";
+		
+    	//2
+//    	String s = "leetcode";
+    	
+    	// -1
+//    	String croakOfFrogs = "1229857369";
+    	
+    	// -1
+//    	String croakOfFrogs = "covid2019";
+    	
+    	// -1
+//    	String croakOfFrogs = "ab123";
+    	
+		System.out.println(new Solution().reformat(s));
+	}
+}
---
+++ src/main/java/com/jasper/algs/leetcode/greedy/qu1417/solu1/Solution.java
@@ -0,0 +1,82 @@
+package com.jasper.algs.leetcode.greedy.qu1417.solu1;
+
+/**
+ * 1417. 重新格式化字符串
+ * 
+ * <p> 贪心算法
+ */
+class Solution {
+    public String reformat(String s) {
+
+    	int count = s.length();
+    	// 边界：两个字母
+    	if(count==2 && !isNumberic(s.charAt(0)) && !isNumberic(s.charAt(0))) 
+    		return "";
+    	
+		int[] ans = new int[count+1];
+    	int offset = 1;
+    	
+    	// 贪心选择
+    	int i = 1; // 数字
+    	int j = 2; // 字母
+    	for (char c : s.toCharArray()) {
+    		// 贪心选择位置
+    		if(isNumberic(c)) {
+    			// 边界
+        		if(i>count) return "";
+        		
+    			ans[i] = c;
+        		i+=2;
+    		} else {
+    			// 边界
+    			if(j>count) 
+        			if(ans[0]==0) {
+        				ans[0] = c;
+        				offset = 0;
+        			}
+        			else return "";
+    			else {
+    				ans[j] = c;
+        			j+=2;
+    			}
+    		}
+		}
+    	
+    	return new String(ans, offset, count);
+    }
+    
+    private boolean isNumberic(char c) {
+    	return c>='0' && c<='9';
+    }
+    
+    public static void main(String[] args) {
+		
+    	// 0a1b2c
+//    	String s = "a0b1c2";
+		
+    	// 空字串
+//    	String s = "leetcode";
+    	
+    	// 空字串
+//    	String s = "1229857369";
+    	
+    	// 
+//    	String s = "covid2019";
+    	
+    	// 1a2b3
+//    	String s = "ab123";
+    	
+    	// a
+//    	String s = "a";
+    	// 2
+//    	String s = "2";
+    	
+    	// 空字串
+//    	String s = "ab";
+    	// 空字串
+    	String s = "12";
+    	
+		System.out.println(new Solution().reformat(s));
+    	
+	}
+}
