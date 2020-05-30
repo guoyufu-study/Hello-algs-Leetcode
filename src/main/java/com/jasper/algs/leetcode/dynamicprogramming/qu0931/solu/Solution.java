204597f0a0a0f392f0b367177c0e1a4e76b0b8a1
guoyufu-study <guoyufu_study@163.com>
2020-05-30 11:50:20 +0800

    0931.降路径最小和 => 动态规划

---
+++ src/main/java/com/jasper/algs/leetcode/dynamicprogramming/qu0931/solu/Solution.java
@@ -0,0 +1,20 @@
+package com.jasper.algs.leetcode.dynamicprogramming.qu0931.solu;
+
+/**
+ * 0931. 下降路径最小和
+ * 
+ * <p> 2刷
+ */
+class Solution {
+    public int minFallingPathSum(int[][] A) {
+
+    	return 0;
+    }
+    
+    public static void main(String[] args) {
+    	//  12
+		int[][] A = new int[][] {{1,2,3},{4,5,6},{7,8,9}};
+		
+		System.out.println(new Solution().minFallingPathSum(A));
+	}
+}
