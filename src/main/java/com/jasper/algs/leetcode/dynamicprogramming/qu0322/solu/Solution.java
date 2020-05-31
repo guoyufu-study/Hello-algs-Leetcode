85a44b11b6c8692217c62e19c28f48b9df8d6510
guoyufu-study <guoyufu_study@163.com>
2020-05-31 18:32:47 +0800

    0322.零钱兑换 => 动态规划

---
+++ src/main/java/com/jasper/algs/leetcode/dynamicprogramming/qu0322/solu/Solution.java
@@ -0,0 +1,25 @@
+package com.jasper.algs.leetcode.dynamicprogramming.qu0322.solu;
+
+/**
+ * 0322.零钱兑换
+ * 
+ * <p> 2 刷
+ */
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+
+    	return 0;
+    }
+    
+    public static void main(String[] args) {
+		// 3
+//    	int[] coins = new int[] {1, 2, 5};
+//		int amount = 11;
+		
+		// -1
+		int[] coins = new int[] {2};
+		int amount = 3;
+		
+		System.out.println(new Solution().coinChange(coins, amount));
+	}
+}
---
+++ src/main/java/com/jasper/algs/leetcode/dynamicprogramming/qu0322/solu1/Solution.java
@@ -0,0 +1,65 @@
+package com.jasper.algs.leetcode.dynamicprogramming.qu0322.solu1;
+
+/**
+ * 0322.零钱兑换
+ * 
+ * <p> 动态规划：自上而下
+ */
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+
+    	// 边界
+    	if(coins==null || coins.length==0 || amount<0)
+    		return -1;
+    	
+    	// 自上而下动态规划
+    	return coinChange(coins, amount, new int[amount+1]);
+    	
+    }
+    
+    private int coinChange(int[] coins, int rem, int[] dp) {
+    	// 边界
+    	if (rem < 0) return -1;
+        if (rem == 0) return 0;
+        
+        // 使用缓存
+        if (dp[rem] != 0) 
+        	return dp[rem];
+        
+        // 计算
+        int min = Integer.MAX_VALUE;
+        for (int coin : coins) {
+          int res = coinChange(coins, rem - coin, dp);
+          if (res >= 0 && res < min)
+            min = 1 + res;
+        }
+        
+        // 缓存
+        dp[rem] = (min == Integer.MAX_VALUE) ? -1 : min;
+        
+        return dp[rem];
+
+	}
+
+	public static void main(String[] args) {
+		// 3
+//    	int[] coins = new int[] {1, 2, 5};
+//		int amount = 11;
+		
+		// -1
+//		int[] coins = new int[] {2};
+//		int amount = 3;
+		
+		// 注意边界
+		// 0
+//    	int[] coins = new int[] {1};
+//		int amount = 0;
+    	
+		// 20
+    	int[] coins = new int[] {186,419,83,408};
+    	int amount = 6249;
+
+		
+		System.out.println(new Solution().coinChange(coins, amount));
+	}
+}
