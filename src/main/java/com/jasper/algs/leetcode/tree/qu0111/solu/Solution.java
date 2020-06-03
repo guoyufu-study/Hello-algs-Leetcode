6ee639d68c8d4b6f6f75c0755092fbc5b73bd3b2
guoyufu-study <guoyufu_study@163.com>
2020-06-03 16:17:21 +0800

    0111. 二叉树的最小深度 => DFS

---
+++ src/main/java/com/jasper/algs/leetcode/tree/qu0111/solu/Solution.java
@@ -0,0 +1,26 @@
+package com.jasper.algs.leetcode.tree.qu0111.solu;
+
+import com.jasper.algs.leetcode.tree.TreeNode;
+
+/**
+ * 0111. 二叉树的最小深度
+ * 
+ * <p> 2刷
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+
+    	return 0;
+    }
+    
+    public static void main(String[] args) {
+    	// 2
+    	TreeNode root = new TreeNode(3);
+    	root.left = new TreeNode(9);
+    	root.right = new TreeNode(20);
+    	root.right.left = new TreeNode(15);
+    	root.right.right = new TreeNode(7);
+    	
+		System.out.println(new Solution().minDepth(root));
+	}
+}
