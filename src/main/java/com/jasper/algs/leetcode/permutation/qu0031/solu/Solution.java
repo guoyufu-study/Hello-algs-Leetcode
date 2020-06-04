147db105f5a097623d106a614a8ed3aa9a548b84
guoyufu-study <guoyufu_study@163.com>
2020-06-04 23:18:16 +0800

    0031. 下一个排列

---
+++ src/main/java/com/jasper/algs/leetcode/permutation/qu0031/solu/Solution.java
@@ -0,0 +1,21 @@
+package com.jasper.algs.leetcode.permutation.qu0031.solu;
+
+/**
+ * 0031. 下一个排列
+ * 
+ *  <p> 2刷
+ */
+class Solution {
+    public void nextPermutation(int[] nums) {
+    	
+    }
+    
+    public static void main(String[] args) {
+//		int[] nums = new int[] {3,2,1};
+    	int[] nums = new int[] {2,3,1};
+//		int[] nums = new int[] {1,2,3};
+		
+		
+		new Solution().nextPermutation(nums);
+	}
+}
