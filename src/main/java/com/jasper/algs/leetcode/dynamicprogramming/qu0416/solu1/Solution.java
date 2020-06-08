20f16115d55f37f3bcc9f0e2eb0948aa38d32e05
guoyufu-study <guoyufu_study@163.com>
2020-06-08 23:26:29 +0800

    0416. 分割等和子集 => 0/1背包问题

---
+++ src/main/java/com/jasper/algs/leetcode/dynamicprogramming/qu0416/solu1/Solution.java
@@ -0,0 +1,46 @@
+package com.jasper.algs.leetcode.dynamicprogramming.qu0416.solu1;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 0416. 分割等和子集
+ * 
+ * <p> 0/1背包
+ */
+class Solution {
+    public boolean canPartition(int[] nums) {
+
+    	int sum = IntStream.of(nums).sum();
+    	// 位操作判断奇偶
+    	if((sum & 1) == 1) return false;
+    	
+    	// 转换成01背包问题
+    	sum >>= 1;
+    	boolean[] dp = new boolean[sum+1];
+    	// 什么都不选
+        Arrays.fill(dp, false);
+        dp[0] = true;
+    	
+        for (int num : nums) {
+            for (int i = sum; i >= num; i--) {
+                dp[i] = dp[i] // 不选
+                		|| dp[i-num]; // 选
+            }
+        }
+        return dp[sum];
+    }
+    
+    public static void main(String[] args) {
+		int[][] nums = new int[][] {
+			{1, 5, 11, 5}, // true
+			{1, 2, 3, 5}, // false
+		};
+		
+		Arrays.stream(nums)
+			.map(new Solution()::canPartition)
+			.collect(Collectors.toList())
+			.forEach(System.out::println);
+	}
+}
