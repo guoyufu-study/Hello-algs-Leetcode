1046d3299c4e88dcc3152387a4b92c3585dd17c2
guoyufu-study <guoyufu_study@163.com>
2020-06-09 20:07:53 +0800

    0416. 分割等和子集

--- src/main/java/com/jasper/algs/leetcode/dynamicprogramming/qu0416/solu1/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/dynamicprogramming/qu0416/solu1/Solution.java
@@ -7,29 +7,33 @@ import java.util.stream.IntStream;
 /**
  * 0416. 分割等和子集
  * 
- * <p> 0/1背包
+ * <p> 0/1背包：二维
  */
 class Solution {
     public boolean canPartition(int[] nums) {
 
     	int sum = IntStream.of(nums).sum();
-    	// 位操作判断奇偶
-    	if((sum & 1) == 1) return false;
-    	
-    	// 转换成01背包问题
-    	sum >>= 1;
-    	boolean[] dp = new boolean[sum+1];
-    	// 什么都不选
-        Arrays.fill(dp, false);
-        dp[0] = true;
-    	
-        for (int num : nums) {
-            for (int i = sum; i >= num; i--) {
-                dp[i] = dp[i] // 不选
-                		|| dp[i-num]; // 选
+        if((sum & 1) == 1) return false;
+
+        sum >>= 1;
+
+        int n = nums.length;
+        boolean[][] dp = new boolean[n+1][sum+1];
+        dp[0][0] = true;
+
+        for(int i=1; i<=n; i++) {
+            int m = Math.min(nums[i-1], sum+1);
+            for(int j=0; j<m; j++) {
+                dp[i][j] = dp[i-1][j];
+            }
+
+            for(int j=nums[i-1]; j<=sum; j++) {
+                dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
             }
         }
-        return dp[sum];
+
+        return dp[n][sum];
+
     }
     
     public static void main(String[] args) {
