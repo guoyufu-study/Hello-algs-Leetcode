50af8b88e569855d17ae20a3c5c31efc4b88bd79
guoyufu-study <guoyufu_study@163.com>
2020-06-09 21:58:13 +0800

    1137.第 N 个泰波那契数

--- src/main/java/com/jasper/algs/leetcode/maths/qu1137/solu/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/maths/qu1137/solu/Solution.java
@@ -1,14 +1,14 @@
-package com.jasper.algs.leetcode.maths.qu1137.solu2;
+package com.jasper.algs.leetcode.maths.qu1137.solu;
 
 /**
  * 泰波那契数
+ * 
  * <p>
- * 矩阵法
+ * 3刷
  */
 class Solution {
     public int tribonacci(int n) {
-    	
+
     	return 0;
     }
-    
 }
--- src/main/java/com/jasper/algs/leetcode/maths/qu1137/solu1/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/maths/qu1137/solu1/Solution.java
@@ -6,7 +6,17 @@ package com.jasper.algs.leetcode.maths.qu1137.solu1;
  * 定义法
  */
 class Solution {
-    public int tribonacci(int n) {
+	// 简化代码
+	public int tribonacci(int n) {
+
+		int[] dp = { 0, 1, 1 };
+        for (int i = 3; i <= n; i++) {
+            dp[i % 3] = dp[i % 3] + dp[(i - 1) % 3] + dp[(i - 2) % 3];
+        }
+        return dp[n % 3];
+    }
+	
+    public int tribonacci2(int n) {
 
     	// 边界值
     	if(n<2) return n;
