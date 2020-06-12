bba9241342cf2a5b302d7834baf38a2a96e69a1d
guoyufu-study <guoyufu_study@163.com>
2020-06-12 14:16:14 +0800

    0009. 回文数

--- src/main/java/com/jasper/algs/leetcode/maths/qu0009/solu3/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/maths/qu0009/solu3/Solution.java
@@ -8,14 +8,9 @@ package com.jasper.algs.leetcode.maths.qu0009.solu3;
 class Solution {
     public boolean isPalindrome(int x) {
         
-    	// 特殊情况：
-        // 如上所述，当 x < 0 时，x 不是回文数。
-        // 同样地，如果数字的最后一位是 0，为了使该数字为回文，
-        // 则其第一位数字也应该是 0
-        // 只有 0 满足这一属性
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
-        }
+    	// 边界值处理
+    	if(x==0) return true; // 0
+        if(x<0 || x%10==0) return false; // 负数、10的倍数
 
         int revertedNumber = 0;
         while (x > revertedNumber) {
