b1bdb7ff3c5e86e3dfd79458a7258f3bbf3389a1
guoyufu-study <guoyufu_study@163.com>
2020-08-05 00:26:07 +0800

    0001.两数之和

---
+++ src/main/java/com/jasper/algs/leetcode/hashmap/qu0001/solu/Solution.java
@@ -0,0 +1,13 @@
+package com.jasper.algs.leetcode.hashmap.qu0001.solu;
+
+/**
+ * 0001.两数之和
+ * <p> 4刷
+ */
+public class Solution {
+
+	public int[] twoSum(int[] nums, int target) {
+		
+		throw new IllegalArgumentException("No two sum solution");
+	}
+}
