b7e3bfe328c55efc6506579b2d6babea25cedf4f
guoyufu-study <guoyufu_study@163.com>
2020-08-06 20:46:03 +0800

    0002.两大数相加（逆序链表）- 测试

--- src/test/java/com/jasper/algs/leetcode/maths/qu0002/solu1/SolutionTests.java
+++ src/test/java/com/jasper/algs/leetcode/maths/qu0002/solu1/SolutionTests.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import com.jasper.algs.leetcode.maths.qu0002.ListNode;
+import com.jasper.algs.leetcode.linkedlist.ListNode;
 
 public class SolutionTests {
 
