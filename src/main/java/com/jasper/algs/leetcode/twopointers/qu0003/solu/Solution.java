b81b0ab21c15552f3559371122a42f9a7551f058
guoyufu-study <guoyufu_study@163.com>
2020-08-07 01:29:53 +0800

    0003.最长无重复字符子串

--- src/main/java/com/jasper/algs/leetcode/twopointers/qu0003/solu/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/twopointers/qu0003/solu/Solution.java
@@ -4,24 +4,24 @@ package com.jasper.algs.leetcode.twopointers.qu0003.solu;
  * 0003.最长无重复字符子串
  * 
  * <p>
- * 4刷
+ * 5刷
  */
 public class Solution {
 
 	public int lengthOfLongestSubstring(String s) {
-
+		
 		return -1;
 	}
 
 	public static void main(String[] args) {
 		// 3
-		String s = "pwwkew";
+//		String s = "pwwkew";
 
 		// 1
 //		String s = "bbbbb";
 
 		// 3
-//		String s = "abcabcbb";
+		String s = "abcabcbb";
 
 		System.out.println(new Solution().lengthOfLongestSubstring(s));
 	}
