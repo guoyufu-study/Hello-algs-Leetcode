2361675f23ed7346f768d5ece3d2b0db9603b0ac
guoyufu-study <guoyufu_study@163.com>
2020-08-08 14:17:14 +0800

    剑指 Offer 22. 链表中倒数第k个节点、面试题 02.02.返回倒数第 k 个节点、0019.删除链表的倒数第N个节点

--- src/main/java/com/jasper/algs/leetcode/linkedlist/qu0019/solu/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/linkedlist/qu0019/solu/Solution.java
@@ -5,7 +5,7 @@ import com.jasper.algs.leetcode.linkedlist.ListNode;
 /**
  * 0019. 删除链表的倒数第N个节点
  *
- * <p> 三刷
+ * <p> 5刷
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
