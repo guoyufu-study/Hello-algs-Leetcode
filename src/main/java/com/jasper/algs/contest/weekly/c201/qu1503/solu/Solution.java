3dafa21a68156b9f2691eded45038fbfe0dba39a
guoyufu-study <guoyufu_study@163.com>
2020-08-09 13:55:27 +0800

    1503. 整理字符串 => 辅助栈

---
+++ src/main/java/com/jasper/algs/contest/weekly/c201/qu1503/solu/Solution.java
@@ -0,0 +1,26 @@
+package com.jasper.algs.contest.weekly.c201.qu1503.solu;
+
+/**
+ * 1503. 整理字符串
+ * 
+ * <p> 3刷
+ */
+class Solution {
+    public String makeGood(String s) {
+
+    	return "";
+    }
+    
+    public static void main(String[] args) {
+    	// leetcode
+//		String s = "leEeetcode";
+		
+		// 空字符串
+//		String s = "abBAcC";
+		
+		// S
+    	String s = "s";
+    	
+		System.out.println(new Solution().makeGood(s));
+	}
+}
---
+++ src/main/java/com/jasper/algs/contest/weekly/c201/qu1503/solu1/Solution.java
@@ -0,0 +1,41 @@
+package com.jasper.algs.contest.weekly.c201.qu1503.solu1;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 1503. 整理字符串
+ * 
+ * <p> 辅助栈
+ */
+class Solution {
+    public String makeGood(String s) {
+    	Deque<Character> stack = new LinkedList<Character>();
+        for(char c : s.toCharArray()) {
+            if(!stack.isEmpty() && c != stack.peek() && Character.toUpperCase(c) == Character.toUpperCase(stack.peek())) {
+            	stack.pop();// 消除
+            } else {
+            	stack.push(c);// 记忆
+            }
+        }
+        
+        StringBuilder ans = new StringBuilder();
+        while(!stack.isEmpty()) {
+        	ans.append(stack.pollLast());
+        }
+        return ans.toString();
+    }
+    
+    public static void main(String[] args) {
+    	// leetcode
+		String s = "leEeetcode";
+		
+		// 空字符串
+//		String s = "abBAcC";
+		
+		// S
+//    	String s = "s";
+    	
+		System.out.println(new Solution().makeGood(s));
+	}
+}
