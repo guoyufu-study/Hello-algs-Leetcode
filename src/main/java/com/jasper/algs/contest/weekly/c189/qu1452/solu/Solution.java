e4a084d0ee5fd670fe85534e259410421c3c07a9
guoyufu-study <guoyufu_study@163.com>
2020-08-09 18:36:53 +0800

    1452. 收藏清单

---
+++ src/main/java/com/jasper/algs/contest/weekly/c189/qu1452/solu/Solution.java
@@ -0,0 +1,15 @@
+package com.jasper.algs.contest.weekly.c189.qu1452.solu;
+
+import java.util.List;
+
+/**
+ * 1452. 收藏清单
+ * 
+ * <p> 3刷
+ */
+class Solution {
+    public List<Integer> peopleIndexes(List<List<String>> favoriteCompanies) {
+
+    	return null;
+    }
+}
