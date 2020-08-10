4a29567cc7d5b36dea448a247105bb8849fe73fc
guoyufu-study <guoyufu_study@163.com>
2020-08-10 14:12:05 +0800

    程序员面试金典第6版 => 第 2 章 整理

--- src/main/java/com/jasper/algs/interview/qu0201/solu/Solution.java
+++ src/main/java/com/jasper/algs/interview/qu0201/solu/Solution.java
@@ -1,11 +1,11 @@
-package com.jasper.algs.interview.qu1495.solu;
+package com.jasper.algs.interview.qu0201.solu;
 
 import com.jasper.algs.leetcode.linkedlist.ListNode;
 
 /**
- * 1495.面试题 02.01. 移除重复节点
+ * 面试题 02.01. 移除重复节点
  * 
- * <p> 2 刷
+ * <p> 3 刷
  */
 class Solution {
     public ListNode removeDuplicateNodes(ListNode head) {
--- src/main/java/com/jasper/algs/interview/qu0201/solu1/Solution.java
+++ src/main/java/com/jasper/algs/interview/qu0201/solu1/Solution.java
@@ -1,9 +1,9 @@
-package com.jasper.algs.interview.qu1495.solu1;
+package com.jasper.algs.interview.qu0201.solu1;
 
 import com.jasper.algs.leetcode.linkedlist.ListNode;
 
 /**
- * 1495.面试题 02.01. 移除重复节点
+ * 面试题 02.01. 移除重复节点
  * 
  * <p> 辅助字典
  */
