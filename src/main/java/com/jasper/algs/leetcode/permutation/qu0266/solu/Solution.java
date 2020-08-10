8ca37ebb28f48ddf4654b5f9906bde42fb2ee4d8
guoyufu-study <guoyufu_study@163.com>
2020-08-10 11:06:21 +0800

    面试题 01.04.回文排列

--- src/main/java/com/jasper/algs/leetcode/permutation/qu0266/solu/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/permutation/qu0266/solu/Solution.java
@@ -3,7 +3,7 @@ package com.jasper.algs.leetcode.permutation.qu0266.solu;
 /**
  * 0266. 回文排列
  * 
- *  <p> 2刷
+ *  <p> 5刷
  */
 class Solution {
     public boolean canPermutePalindrome(String s) {
--- src/main/java/com/jasper/algs/leetcode/permutation/qu0266/solu1/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/permutation/qu0266/solu1/Solution.java
@@ -3,19 +3,19 @@ package com.jasper.algs.leetcode.permutation.qu0266.solu1;
 /**
  * 0266. 回文排列
  * 
- *  <p> 辅助数组
+ *  <p> 辅助数组记数
  */
 class Solution {
     public boolean canPermutePalindrome(String s) {
 
-    	int[] map = new int[128];
+    	int[] helper = new int[128];
         for (int i = 0; i < s.length(); i++) {
-            map[s.charAt(i)]++;
+            helper[s.charAt(i)]++;
         }
         
         int count = 0;
-        for (int key = 0; key < map.length && count <= 1; key++) {
-            count += map[key] % 2;
+        for (int key = 0; key < helper.length && count <= 1; key++) {
+            count += helper[key] % 2;
         }
         return count <= 1;
 
