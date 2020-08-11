1ae1ce41976f770b4132db11afaab9d6143f4b21
guoyufu-study <guoyufu_study@163.com>
2020-08-11 18:47:51 +0800

    0442. 数组中重复的数据

---
+++ src/main/java/com/jasper/algs/leetcode/bitmanipulation/qu0442/solu/Solution.java
@@ -0,0 +1,17 @@
+package com.jasper.algs.leetcode.bitmanipulation.qu0442.solu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 0442. 数组中重复的数据
+ * 
+ * <p> 3 刷
+ */
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+    	ArrayList<Integer> ans = new ArrayList<>();
+    	
+    	return ans;
+    }
+}
