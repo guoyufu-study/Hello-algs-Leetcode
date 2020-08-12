f466177da3497d6dc40f371f8c32ea9d594d315a
guoyufu-study <guoyufu_study@163.com>
2020-08-12 12:43:33 +0800

    0657. 机器人能否返回原点

---
+++ src/main/java/com/jasper/algs/leetcode/string/qu0657/solu/Solution.java
@@ -0,0 +1,13 @@
+package com.jasper.algs.leetcode.string.qu0657.solu;
+
+/**
+ * 0657. 机器人能否返回原点
+ * 
+ * <p> 3 刷
+ */
+class Solution {
+    public boolean judgeCircle(String moves) {
+
+    	return false;
+    }
+}
---
+++ src/main/java/com/jasper/algs/leetcode/string/qu0657/solu1/Solution.java
@@ -0,0 +1,34 @@
+package com.jasper.algs.leetcode.string.qu0657.solu1;
+
+/**
+ * 0657. 机器人能否返回原点
+ * 
+ * <p> 计数相消
+ */
+class Solution {
+    public boolean judgeCircle(String moves) {
+    	int n = moves.length();
+    	if(n%2==1)
+    		return false;
+    	
+    	int x = 0, y = 0;
+    	for (char c : moves.toCharArray()) {
+			switch (c) {
+			case 'R':
+				x++;
+				break;
+			case 'L':
+				x--;
+				break;
+			case 'U':
+				y++;
+				break;
+			case 'D':
+				y--;
+				break;
+			}
+		}
+    	
+    	return x==0 && y==0;
+    }
+}
