c0869ac5abf92e0c1f86401f9579097f0089d409
guoyufu-study <guoyufu_study@163.com>
2020-08-23 01:54:51 +0800

    5482. 二维网格图中探测环 <= 第 33 场双周赛

---
+++ src/main/java/com/jasper/algs/contest/biweekly/bc33/qu5482/solu/Solution.java
@@ -0,0 +1,13 @@
+package com.jasper.algs.contest.biweekly.bc33.qu5482.solu;
+
+/**
+ * 5482. 二维网格图中探测环
+ * 
+ * <p> 3 刷
+ */
+class Solution {
+    public boolean containsCycle(char[][] grid) {
+
+    	return false;
+    }
+}
---
+++ src/main/java/com/jasper/algs/contest/biweekly/bc33/qu5482/solu1/Solution.java
@@ -0,0 +1,49 @@
+package com.jasper.algs.contest.biweekly.bc33.qu5482.solu1;
+
+/**
+ * 5482. 二维网格图中探测环
+ * 
+ * <p> DFS
+ */
+class Solution {
+	
+	int m, n;
+	char[][] grid;
+	int dist = 'a' - 'A';
+	int[] offsetX = new int[] { 0, -1, 1, 0};
+	int[] offsetY = new int[] {-1,  0, 0, 1};
+    public boolean containsCycle(char[][] grid) {
+    	m = grid.length;
+    	n = grid[0].length;
+    	this.grid = grid;
+    	
+    	if(m==1 || n==1) return false;
+    	for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if(grid[i][j]<'a') continue;
+				if(dfs(i, j)) 
+					return true;
+			}
+		}
+    	
+    	return false;
+    }
+	private boolean dfs(int i, int j) {
+		char prev = grid[i][j];
+		grid[i][j] -= dist; // 改成大写
+		int count = 0;
+		for (int k = 0; k < 4; k++) {
+			int x = i + offsetX[k];
+			int y = j + offsetY[k];
+			if(x>=0 && x<m && y>=0 && y<n) {
+			    if(grid[x][y]==grid[i][j]) count++;
+                else if(grid[x][y]==prev && dfs(x,y))
+                    return true;
+			}
+		}
+		
+		if(count>=2) return true;
+		
+		return false;
+	}
+}
