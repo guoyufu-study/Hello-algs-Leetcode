a3e7843887c6b6557bc1c7d3bcf3f60163f2e5ca
guoyufu-study <guoyufu_study@163.com>
2020-08-31 02:25:11 +0800

    0840. 矩阵中的幻方 <= 第 086 场周赛

---
+++ src/main/java/com/jasper/algs/contest/weekly/c086/qu0840/solu/Solution.java
@@ -0,0 +1,13 @@
+package com.jasper.algs.contest.weekly.c086.qu0840.solu;
+
+/**
+ * 0840. 矩阵中的幻方
+ * 
+ * <p> 3 刷
+ */
+class Solution {
+    public int numMagicSquaresInside(int[][] grid) {
+
+    	return 0;
+    }
+}
