39f79f32658455ba95eef168923456e2a26c4e51
guoyufu-study <guoyufu_study@163.com>
2020-08-31 13:10:36 +0800

    1143. 最长公共子序列 <= 动态规划

---
+++ src/main/java/com/jasper/algs/leetcode/dp/qu1143/solu/Solution.java
@@ -0,0 +1,13 @@
+package com.jasper.algs.leetcode.dp.qu1143.solu;
+
+/**
+ * 1143. 最长公共子序列
+ * 
+ * <p> 3 刷
+ */
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+
+    	return 0;
+    }
+}
---
+++ src/main/java/com/jasper/algs/leetcode/dp/qu1143/solu1/Solution.java
@@ -0,0 +1,25 @@
+package com.jasper.algs.leetcode.dp.qu1143.solu1;
+
+/**
+ * 1143. 最长公共子序列
+ * 
+ * <p> 动态规划
+ */
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+    	int m = text1.length(), n = text2.length();
+    	int[][] dp = new int[m+1][n+1];
+    	
+    	for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+				if(text1.charAt(i-1)==text2.charAt(j-1)) {
+					dp[i][j] = dp[i-1][j-1] + 1;
+				} else {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+				}
+			}
+		}
+    	
+    	return dp[m][n];
+    }
+}
