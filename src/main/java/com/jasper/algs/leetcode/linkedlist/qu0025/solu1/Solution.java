0fa3e4a4b16bd58baab9c6b1a123b2580ccf1006
guoyufu-study <guoyufu_study@163.com>
2020-09-04 14:15:26 +0800

    0025. K 个一组翻转链表

---
+++ src/main/java/com/jasper/algs/leetcode/linkedlist/qu0025/solu1/Solution.java
@@ -0,0 +1,43 @@
+package com.jasper.algs.leetcode.linkedlist.qu0025.solu1;
+
+import com.jasper.algs.leetcode.linkedlist.ListNode;
+
+/**
+ * 0025. K 个一组翻转链表
+ * 
+ * <p> 模拟
+ */
+class Solution {
+	public ListNode reverseKGroup(ListNode head, int k) {
+		
+    	return k==1 ? head : reverse(head, k);
+    }
+
+	private ListNode reverse(ListNode head, int k) {
+		// < k
+		int count = 0;
+		ListNode curr = head;
+		while(curr != null) {
+			curr = curr.next;
+			count++;
+		}
+		if(count < k) return head;
+		
+		// >= k
+		count = 0;
+		curr = head;
+		ListNode prev = null, next = head.next;
+		while(count < k) {
+			curr.next = prev;
+			
+			prev = curr;
+			curr = next;
+			if(next!=null)
+				next = next.next;
+			count++;
+		}
+		
+		head.next = reverse(curr, k);
+		return prev;
+	}
+}
