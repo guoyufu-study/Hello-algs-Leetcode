acd6540b8b6066837a9aa618226063ae4a508ae4
guoyufu-study <guoyufu_study@163.com>
2020-09-13 21:54:22 +0800

    0713. 乘积小于K的子数组

---
+++ src/main/java/com/jasper/algs/leetcode/twopointers/qu0713/solu/Solution.java
@@ -0,0 +1,14 @@
+package com.jasper.algs.leetcode.twopointers.qu0713.solu;
+
+/**
+ * 0713. 乘积小于K的子数组
+ *
+ * <p>
+ * 4 刷
+ */
+class Solution {
+	public int numSubarrayProductLessThanK(int[] nums, int k) {
+
+		return 0;
+	}
+}
