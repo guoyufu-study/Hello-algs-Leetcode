cc2d1a9683a117c97e78015089a9707611d1ba98
guoyufu-study <guoyufu_study@163.com>
2020-09-18 11:52:03 +0800

    0033. 搜索旋转排序数组

--- src/main/java/com/jasper/algs/leetcode/binarysearch/qu0033/solu1/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/binarysearch/qu0033/solu1/Solution.java
@@ -7,22 +7,21 @@ package com.jasper.algs.leetcode.binarysearch.qu0033.solu1;
  */
 class Solution {
     public int search(int[] nums, int target) {
-    	int n = nums.length, left = 0, right = n-1;
-    	while(left <= right) {
-    		int mid = left + (right - left) / 2;
-    		if(nums[mid]==target) return mid;
-    		
-    		if(nums[mid] < target) { // 小了
-    			if(nums[left] <= nums[mid]) left = mid + 1;
-    			else if(nums[left] <= target) right = mid - 1;
-    			else left = mid + 1;
-    		} else { // 大了
-    			if(nums[left] > nums[mid]) right = mid - 1;
-    			else if(nums[left] > target) left = mid + 1;
-    			else right = mid - 1;
-    		}
-    	}
-    	
-    	return -1;
+    	// 二分查找
+        int n = nums.length;
+        int left = 0, right = n-1;
+        while(left <= right) {
+            int pivot = left + (right-left)/2;
+            if(nums[pivot]==target) return pivot; // 找到
+            if(nums[pivot] > nums[right]) { // 前段有序
+                if(nums[left] <= target && target < nums[pivot]) right = pivot - 1; 
+                else left = pivot + 1;
+            } else {// 后段有序
+                if(nums[pivot] < target && target <= nums[right]) left = pivot + 1;
+                else right = pivot - 1;
+            }
+        }
+
+        return -1;
     }
 }
