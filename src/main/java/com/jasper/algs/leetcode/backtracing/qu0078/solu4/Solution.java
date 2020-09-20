da80d2b0cc7a89f11052d65dc6588fa2c52a414d
guoyufu-study <guoyufu_study@163.com>
2020-09-20 21:06:37 +0800

    0078.子集

--- src/main/java/com/jasper/algs/leetcode/backtracing/qu0078/solu4/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/backtracing/qu0078/solu4/Solution.java
@@ -16,15 +16,18 @@ class Solution {
 		List<List<Integer>> ans = new ArrayList<List<Integer>>();
 		int n = nums.length;
 		int max = 1 << n;
-		for (int i = 0; i < max; i++) {
+		for (int mask = 0; mask < max; mask++) {
 			// 长度为 n 的二进制位掩码
-			String bitmask = Integer.toBinaryString(i | max).substring(1);
+//			String bitmask = Integer.toBinaryString(mask | max).substring(1);
 			
 			// 位掩码 -> 子集
 			List<Integer> tmp = new LinkedList<Integer>();
-			for (int j = 0; j < n; j++) 
-				if(bitmask.charAt(j)=='1') tmp.add(nums[j]);
+//			for (int j = 0; j < n; j++) 
+//				if(bitmask.charAt(j)=='1') tmp.add(nums[j]);
 			
+			for (int j = 0; j < n; ++j)
+				if ((mask & (1 << j)) != 0) tmp.add(nums[j]);
+	                	
 			ans.add(tmp);
 		}
 		
