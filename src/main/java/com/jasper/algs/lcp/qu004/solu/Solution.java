17b069f790268388a20a172cc641d7225891cf2e
guoyufu-study <guoyufu_study@163.com>
2020-09-25 19:17:32 +0800

    LCP 04. 覆盖

---
+++ src/main/java/com/jasper/algs/lcp/qu004/solu/Solution.java
@@ -0,0 +1,13 @@
+package com.jasper.algs.lcp.qu004.solu;
+
+/**
+ * LCP 04. 覆盖
+ * 
+ * <p> 6 刷
+ */
+class Solution {
+    public int domino(int n, int m, int[][] broken) {
+
+    	return 0;
+    }
+}
---
+++ src/main/java/com/jasper/algs/lcp/qu004/solu1/Solution.java
@@ -0,0 +1,13 @@
+package com.jasper.algs.lcp.qu004.solu1;
+
+/**
+ * LCP 04. 覆盖
+ * 
+ * <p> 动态规划
+ */
+class Solution {
+    public int domino(int n, int m, int[][] broken) {
+
+    	return 0;
+    }
+}
