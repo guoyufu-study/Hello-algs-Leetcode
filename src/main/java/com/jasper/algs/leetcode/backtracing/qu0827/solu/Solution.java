9fe2a1f1b6b3cd826754453375ed8ee66325adb2
guoyufu-study <guoyufu_study@163.com>
2020-09-25 18:36:58 +0800

    0827. 最大人工岛

--- src/main/java/com/jasper/algs/leetcode/backtracing/qu0827/solu/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/backtracing/qu0827/solu/Solution.java
@@ -3,7 +3,7 @@ package com.jasper.algs.leetcode.backtracing.qu0827.solu;
 /**
  * 0827. 最大人工岛
  *
- * <p> 2刷
+ * <p> 6刷
  */
 class Solution {
     public int largestIsland(int[][] grid) {
--- src/main/java/com/jasper/algs/leetcode/backtracing/qu0827/solu1/Solution.java
+++ src/main/java/com/jasper/algs/leetcode/backtracing/qu0827/solu1/Solution.java
@@ -1,72 +1,57 @@
 package com.jasper.algs.leetcode.backtracing.qu0827.solu1;
 
-import java.util.ArrayList;
+import java.util.Deque;
 import java.util.HashSet;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Set;
 
 /**
  * 0827. 最大人工岛
  *
- * <p> DFS + 字典
+ * <p> 暴力枚举 + DFS
  */
 class Solution {
     int[] dr = new int[]{-1, 0, 1, 0};
     int[] dc = new int[]{0, -1, 0, 1};
-    int[][] grid;
-    int N;
 
     public int largestIsland(int[][] grid) {
-        this.grid = grid;
-        N = grid.length;
-
-        int index = 2;
-        int[] area = new int[N*N + 2];
-        for (int r = 0; r < N; ++r)
-            for (int c = 0; c < N; ++c)
-                if (grid[r][c] == 1)
-                    area[index] = dfs(r, c, index++);
-
+        int N = grid.length;
+        
+        // 暴力枚举 0 + DFS
         int ans = 0;
-        for (int x: area) ans = Math.max(ans, x);
+        boolean hasZero = false;
         for (int r = 0; r < N; ++r)
             for (int c = 0; c < N; ++c)
                 if (grid[r][c] == 0) {
-                    Set<Integer> seen = new HashSet<>();
-                    for (Integer move: neighbors(r, c))
-                        if (grid[move / N][move % N] > 1)
-                            seen.add(grid[move / N][move % N]);
-
-                    int bns = 1;
-                    for (int i: seen) bns += area[i];
-                    ans = Math.max(ans, bns);
+                    hasZero = true;
+                    grid[r][c] = 1;
+                    ans = Math.max(ans, check(grid, r, c));
+                    grid[r][c] = 0;
                 }
 
-        return ans;
+        return hasZero ? ans : N*N;
     }
 
-    public int dfs(int r, int c, int index) {
-        int ans = 1;
-        grid[r][c] = index;
-        for (Integer move: neighbors(r, c)) {
-            if (grid[move / N][move % N] == 1) {
-                grid[move / N][move % N] = index;
-                ans += dfs(move / N, move % N, index);
+    public int check(int[][] grid, int r0, int c0) {
+        int N = grid.length;
+        Deque<Integer> stack = new LinkedList<>();
+        Set<Integer> seen = new HashSet<>();
+        stack.push(r0 * N + c0);
+        seen.add(r0 * N + c0);
+
+        while (!stack.isEmpty()) {
+            int code = stack.pop();
+            int r = code / N, c = code % N;
+            for (int k = 0; k < 4; ++k) {
+                int nr = r + dr[k], nc = c + dc[k];
+                if (!seen.contains(nr * N + nc) && 0 <= nr && nr < N &&
+                        0 <= nc && nc < N && grid[nr][nc] == 1) {
+                    stack.push(nr * N + nc);
+                    seen.add(nr * N + nc);
+                }
             }
         }
 
-        return ans;
-    }
-
-    public List<Integer> neighbors(int r, int c) {
-        List<Integer> ans = new ArrayList<>();
-        for (int k = 0; k < 4; ++k) {
-            int nr = r + dr[k];
-            int nc = c + dc[k];
-            if (0 <= nr && nr < N && 0 <= nc && nc < N)
-                ans.add(nr * N + nc);
-        }
-
-        return ans;
+        return seen.size();
     }
 }
