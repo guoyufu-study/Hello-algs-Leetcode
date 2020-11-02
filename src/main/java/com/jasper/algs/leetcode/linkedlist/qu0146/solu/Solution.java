47234702993d55c4e074db6d8f76e1dff15be253
guoyufu-study <guoyufu_study@163.com>
2020-11-02 12:51:35 +0800

    0146. LRU缓存机制

---
+++ src/main/java/com/jasper/algs/leetcode/linkedlist/qu0146/solu/Solution.java
@@ -0,0 +1,22 @@
+package com.jasper.algs.leetcode.linkedlist.qu0146.solu;
+
+/**
+ * 0146. LRU缓存机制
+ * 
+ * <p> 6 刷
+ */
+class LRUCache {
+
+    public LRUCache(int capacity) {
+
+    }
+    
+    public int get(int key) {
+
+    	return key;
+    }
+    
+    public void put(int key, int value) {
+
+    }
+}
